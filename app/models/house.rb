15c78ba3c1c0733feca536f09240c14e02bd50a6
keiko kondo <keiko.ayaka@icloud.com>
2020-09-10 00:33:31 +0900

    バリデーションの追加

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,8 @@
 class House < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy
   accepts_nested_attributes_for :nearest_stations, allow_destroy: true
+  validates :house_name, presence: true
+  validates :rent_fee, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
 end
